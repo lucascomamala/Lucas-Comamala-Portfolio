efcc38cffb68031c2be8523518e9b0a3625bda40
Lucas Comamala <lucascomamala@gmail.com>
2022-07-18 14:43:45 -0600

    Formatting

--- myScript.js
+++ myScript.js
@@ -1,12 +1,11 @@
-function menuFunction () {
-    const y = document.querySelector('.bars');
-    const nav = document.getElementById('myTopnav');
-    if(nav.className === 'topnav') {
-        nav.classList.add('visible');
-        y.innerHTML('&#10006;');
-    }
-    else{
-        nav.classList.remove('visible');
-        y.innerHTML('&#9776;');
-    }
+function menuFunction() {
+  const y = document.querySelector('.bars');
+  const nav = document.getElementById('myTopnav');
+  if (nav.className === 'topnav') {
+    nav.classList.add('visible');
+    y.innerHTML('&#10006;');
+  } else {
+    nav.classList.remove('visible');
+    y.innerHTML('&#9776;');
+  }
 }
